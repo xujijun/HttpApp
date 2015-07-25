faab2e54ade1afcceacbb5a62c85fd1f559fc44a
Xu <Xu@Xu-PC>
2015-07-25 17:05:35 +0800

    aaa

---
+++ src/main/java/com/xjj/HttpApp/App.java
@@ -0,0 +1,13 @@
+package com.xjj.HttpApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
